6a430789daebc48edd75a6c1d12a8d77ffa0c1ce
Spdearing <142638816+Spdearing@users.noreply.github.com>
2024-02-07 13:50:29 -0500

    Working on the mainMenu Script

--- Dearing_AdventureProject/Assets/Scripts/UI/MainMenuController.cs
+++ Dearing_AdventureProject/Assets/Scripts/UI/MainMenuController.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class MainMenuController : MonoBehaviour
 {
+   [SerializeField] GameObject[] panels;
+    
+    
+
     // Start is called before the first frame update
     void Start()
     {
