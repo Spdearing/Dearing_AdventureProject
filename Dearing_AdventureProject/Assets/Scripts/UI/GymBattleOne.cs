8a9253008c04e3fa57d04dc0296ca71f2ecaf6e1
Spdearing <142638816+Spdearing@users.noreply.github.com>
2024-02-09 19:53:45 -0500

    Building the Battle Scenes

--- Dearing_AdventureProject/Assets/Scripts/UI/GymBattleOne.cs
+++ Dearing_AdventureProject/Assets/Scripts/UI/GymBattleOne.cs
@@ -1,13 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
-public class CombatUIManager : MonoBehaviour
+public class GymBattleOne : MonoBehaviour
 {
     [SerializeField] GameObject mainCombatPanel;
     [SerializeField] GameObject fightPanel;
     [SerializeField] GameObject confirmPanel;
     [SerializeField] GameObject ItemsPanel;
+
+    private TMP_Text enemyName;
+    private TMP_Text playerName;
+    private TMP_Text enemyHealth;
+    private TMP_Text playerHealth;
 
     
 
@@ -17,13 +23,23 @@ public class CombatUIManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        enemyName = GameObject.Find("EnemyName").GetComponent<TMP_Text>();
+        playerName = GameObject.Find("PlayerName").GetComponent<TMP_Text>();
+        enemyHealth = GameObject.Find("EnemyHealth").GetComponent<TMP_Text>();
+        playerHealth = GameObject.Find("PlayerHealth").GetComponent<TMP_Text>();
         panels.Add("MainCombatPanel", mainCombatPanel);
         panels.Add("FightPanel", fightPanel);
-        panels.Add("confirmPanel")
+        panels.Add("confirmPanel", confirmPanel);
     }
     // Update is called once per frame
     void Update()
     {
         
     }
+
+    void UpdateHealthText()
+    {
+
+    }
+
 }
