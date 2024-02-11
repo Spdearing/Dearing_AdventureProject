c334f12d3f61e1987aee69675fc9af14035c88f4
Spdearing <142638816+Spdearing@users.noreply.github.com>
2024-02-11 17:38:04 -0500

    Starting the gym world

--- Dearing_AdventureProject/Assets/Scripts/Player/PlayerMovement.cs
+++ Dearing_AdventureProject/Assets/Scripts/Player/PlayerMovement.cs
@@ -1,18 +1,79 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
+using UnityStandardAssets.CrossPlatformInput;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
 
 public class PlayerMovement : MonoBehaviour
 {
+    /// <summary>
+    /// Player Elements
+    /// </summary>
+    private float walkingSpeed;
+    private float sprintingSpeed;
+    private Rigidbody2D rb;
+    //private Animator animator;
+    private SpriteRenderer spriteRenderer;
+
+    private bool interactable;
+    private TMP_Text interactableText;
+
+
+
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        walkingSpeed = 2.0f;
+        rb = GetComponent<Rigidbody2D>();
+        //animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        interactableText = GameObject.Find("InteractableText").GetComponent<TMP_Text>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float horizontalMovement = CrossPlatformInputManager.GetAxisRaw("Horizontal");
+        float verticalMovement = CrossPlatformInputManager.GetAxisRaw("Vertical");
+
+        // Update horizontal movement
+        rb.velocity = new Vector2(horizontalMovement * walkingSpeed, rb.velocity.y);
+        //animator.SetFloat("movement", Mathf.Abs(horizontalMovement));
+
+        // Update vertical movement
+        rb.velocity = new Vector2(rb.velocity.x, verticalMovement * walkingSpeed);
+
+        if (CrossPlatformInputManager.GetButtonDown("InteractButton") && interactable)
+        {
+            SceneManager.LoadScene("GymBattleOne");
+        }
+
+        //if (horizontalMovement < 0)
+        //{
+        //    spriteRenderer.flipX = true; 
+        //}
+        //else if (horizontalMovement > 0)
+        //{
+        //    spriteRenderer.flipX = false; 
+        //}
+
+        Debug.Log(interactable);
+    }
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("GymTrainerOne"))
+        {
+            interactable = true;
+            interactableText.text = "Talk to the gym member!";
+        }
+        else
+        {
+            interactable = false;
+        }
     }
 }
