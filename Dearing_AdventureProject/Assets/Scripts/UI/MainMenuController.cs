69073429170c78e324ba84bc4e78b8cdf061e9ce
Spdearing <142638816+Spdearing@users.noreply.github.com>
2024-02-27 18:23:01 -0500

    Main menu done

--- Dearing_AdventureProject/Assets/Scripts/UI/MainMenuController.cs
+++ Dearing_AdventureProject/Assets/Scripts/UI/MainMenuController.cs
@@ -1,22 +1,74 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenuController : MonoBehaviour
 {
-   [SerializeField] GameObject[] panels;
-    
-    
+    [SerializeField] GameObject titlePanel;
+    [SerializeField] GameObject settingsPanel;
+    [SerializeField] GameObject questPanel;
 
-    // Start is called before the first frame update
+
+
+
+    Dictionary<string, GameObject> panels = new Dictionary<string, GameObject>();
+
     void Start()
     {
-        
+        panels.Add("TitlePanel", titlePanel);
+        panels.Add("SettingsPanel", settingsPanel);
+        panels.Add("QuestPanel", questPanel);
+
+        SwitchPanel("TitlePanel");
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+
+    void SwitchPanel(string panelName)
+    {
+        string currentPanelName;
+        foreach (var panel in panels)
+        {
+            if (panel.Key == panelName)
+            {
+                panel.Value.SetActive(true);
+                currentPanelName = panelName;
+            }
+            else
+            {
+                panel.Value.SetActive(false);
+            }
+        }
+    }
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Gym");
+    }
+
+    public void BackToMainMenu()
+    {
+        SwitchPanel("TitlePanel");
+    }
+
+    public void SwitchToQuestPanel()
+    {
+        SwitchPanel("QuestPanel");
+    }
+    public void SwitchToSettingsPanel()
+    {
+        SwitchPanel("SettingsPanel");
+    }
+
+    public void QUitGame()
+    {
+        Application.Quit();
     }
 }
